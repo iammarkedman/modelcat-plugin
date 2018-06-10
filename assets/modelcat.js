0cb19452226558e352be047eb9d292b892b2ad54
Markku Mäkelä <markku.makela@gmail.com>
2018-06-10 21:36:58 +0300

    Favorites

--- assets/modelcat.js
+++ assets/modelcat.js
@@ -8,7 +8,12 @@ function nano(template, data) {
   });
 }
 
-var ntmpl_searchResult = '<div class="col-4"><a href="{permalink}"><img src="{mainthumb}" class="img-fluid"/><div class="name">{name}</div></a></div>';
+var ntmpl_searchResult = 
+  '<div class="col-4"><div class="item-holder"><a href="{permalink}">' +
+    '<img src="{mainthumb}" class="img-fluid"/>' + 
+    '<div class="name">{name}</div></a>' +
+    '<div class="heart" data-id="{id}"><i class="far fa-heart"></i></div>' +
+  '</div></div>';
 
 var modelcatSearchDefaultSettings = {
   form: "#searchForm"
@@ -18,6 +23,67 @@ var modelcatSearchDefaultSettings = {
  * Modelcat
  */
 (function($) {
+
+  /**
+   * Bind add/remote from favorites
+   */
+  $.fn.bindFavorite = function() {
+    return this.each(function() {
+      var $root = $(this);
+
+      $root.click(function() {
+        var favs = JSON.parse(localStorage.getItem("fav"));
+        var id = $root.data("id");
+        var idx = favs.indexOf(id);
+        if( idx != -1 ) {
+          favs.splice(idx, 1);
+          localStorage.setItem("fav", JSON.stringify(favs));
+          $root.find("i").removeClass("fas").addClass("far");
+        } else {
+          favs.push(id);
+          localStorage.setItem("fav", JSON.stringify(favs));
+          $root.find("i").removeClass("far").addClass("fas");
+        }
+      });
+    });
+  }
+
+  /**
+   * Bind add/remote from favorites for a single model
+   */
+  $.fn.bindSingleFavorite = function() {
+    return this.each(function() {
+      var $root = $(this);
+
+      var favs = JSON.parse(localStorage.getItem("fav"));
+      var id = $root.data("id");
+      var idx = favs.indexOf(id);
+      var inFavs = ( idx != -1 );
+
+      if( inFavs ) {
+        $root.html('<i class="fas fa-heart"></i> Remove from favorites');
+      } else {
+        $root.html('<i class="far fa-heart"></i> Add to favorites');
+      }
+
+      $root.click(function() {
+        var favs = JSON.parse(localStorage.getItem("fav"));
+        var id = $root.data("id");
+        var idx = favs.indexOf(id);
+        var inFavs = ( idx != -1 );
+
+        if( inFavs ) {
+          favs.splice(idx, 1);
+          localStorage.setItem("fav", JSON.stringify(favs));
+          $root.html('<i class="far fa-heart"></i> Add to favorites');
+        } else {
+          favs.push(id);
+          localStorage.setItem("fav", JSON.stringify(favs));
+          $root.html('<i class="fas fa-heart"></i> Remove from favorites');
+        }
+      });
+    });
+  }
 
   /**
    * Render search results
@@ -49,6 +115,18 @@ var modelcatSearchDefaultSettings = {
         $html += '</div>';
         $root.append($html);
       }
+
+      // check favorite states
+      var favs = JSON.parse(localStorage.getItem("fav"));
+      $root.find(".heart").each( function() {
+        var id = $(this).data("id");
+        if( favs.includes(id) ) {
+          $(this).find("i").removeClass("far").addClass("fas");
+        }
+      });
+
+      // bind hearts
+      $root.find(".heart").bindFavorite();
     });
   }
 
